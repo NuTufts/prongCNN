0a6400d452be1b409a426281af4f0ca0a71bf6e4
Matthew Rosenberg <rosenberg6m@gmail.com>
2023-02-02 00:22:05 -0500

    added option to use 3-layer MLPs for particle classification and completeness regression tasks

--- models/models_instanceNorm_reco_2chan_multiTask.py
+++ models/models_instanceNorm_reco_2chan_multiTask.py
@@ -210,6 +210,206 @@ class ResNet34ClCmp(nn.Module):
 
 
 
+
+class ResNet34DeepMLP(nn.Module):
+    def __init__(self, in_channels, resblock, outputs=6):
+        super().__init__()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.InstanceNorm2d(64, track_running_stats=False, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer1 = nn.Sequential(
+            resblock(64, 64, downsample=False),
+            resblock(64, 64, downsample=False),
+            resblock(64, 64, downsample=False)
+        )
+
+        self.layer2 = nn.Sequential(
+            resblock(64, 128, downsample=True),
+            resblock(128, 128, downsample=False),
+            resblock(128, 128, downsample=False),
+            resblock(128, 128, downsample=False)
+        )
+
+        self.layer3 = nn.Sequential(
+            resblock(128, 256, downsample=True),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False)
+        )
+
+
+        self.layer4 = nn.Sequential(
+            resblock(256, 512, downsample=True),
+            resblock(512, 512, downsample=False),
+            resblock(512, 512, downsample=False)
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+
+        self.fcClass1 = nn.Linear(1536, 1024)
+        self.fcClass2 = nn.Linear(1024, 512)
+        self.fcClass3 = nn.Linear(512, outputs)
+
+        self.fcReg1 = nn.Linear(1536, 1024)
+        self.fcReg2 = nn.Linear(1024, 512)
+        self.fcReg3 = nn.Linear(512, 1)
+        
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+
+    def forward(self, X):
+
+        X0 = X[:,0:2].reshape(X.shape[0], 2, 512, 512)
+        X1 = X[:,2:4].reshape(X.shape[0], 2, 512, 512)
+        X2 = X[:,4:].reshape(X.shape[0], 2, 512, 512)
+
+        X0 = self.layer0(X0)
+        X0 = self.layer1(X0)
+        X0 = self.layer2(X0)
+        X0 = self.layer3(X0)
+        X0 = self.layer4(X0)
+        X0 = self.gap(X0)
+        X0 = X0.reshape(X0.shape[0],512)
+
+        X1 = self.layer0(X1)
+        X1 = self.layer1(X1)
+        X1 = self.layer2(X1)
+        X1 = self.layer3(X1)
+        X1 = self.layer4(X1)
+        X1 = self.gap(X1)
+        X1 = X1.reshape(X1.shape[0],512)
+
+        X2 = self.layer0(X2)
+        X2 = self.layer1(X2)
+        X2 = self.layer2(X2)
+        X2 = self.layer3(X2)
+        X2 = self.layer4(X2)
+        X2 = self.gap(X2)
+        X2 = X2.reshape(X2.shape[0],512)
+
+        X = torch.cat((X0, X1, X2), 1)
+
+        Xclass = nn.ReLU()(self.fcClass1(X))
+        Xclass = nn.ReLU()(self.fcClass2(Xclass))
+        Xclass = self.fcClass3(Xclass)
+
+        Xreg = nn.ReLU()(self.fcReg1(X))
+        Xreg = nn.ReLU()(self.fcReg2(Xreg))
+        Xreg = self.fcReg3(Xreg)
+
+        outputs = [self.logSoftmax(Xclass), nn.Sigmoid()(Xreg).squeeze()]
+        return outputs
+
+
+
+class ResNet34DeepMLPwBN(nn.Module):
+    def __init__(self, in_channels, resblock, outputs=6):
+        super().__init__()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.InstanceNorm2d(64, track_running_stats=False, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer1 = nn.Sequential(
+            resblock(64, 64, downsample=False),
+            resblock(64, 64, downsample=False),
+            resblock(64, 64, downsample=False)
+        )
+
+        self.layer2 = nn.Sequential(
+            resblock(64, 128, downsample=True),
+            resblock(128, 128, downsample=False),
+            resblock(128, 128, downsample=False),
+            resblock(128, 128, downsample=False)
+        )
+
+        self.layer3 = nn.Sequential(
+            resblock(128, 256, downsample=True),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False),
+            resblock(256, 256, downsample=False)
+        )
+
+
+        self.layer4 = nn.Sequential(
+            resblock(256, 512, downsample=True),
+            resblock(512, 512, downsample=False),
+            resblock(512, 512, downsample=False)
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+
+        self.fcClass1 = nn.Linear(1536, 1024)
+        self.fcClass2 = nn.Linear(1024, 512)
+        self.fcClass3 = nn.Linear(512, outputs)
+        self.classBN1 = nn.BatchNorm1d(num_features=1024)
+        self.classBN2 = nn.BatchNorm1d(num_features=512)
+
+        self.fcReg1 = nn.Linear(1536, 1024)
+        self.fcReg2 = nn.Linear(1024, 512)
+        self.fcReg3 = nn.Linear(512, 1)
+        self.regBN1 = nn.BatchNorm1d(num_features=1024)
+        self.regBN2 = nn.BatchNorm1d(num_features=512)
+        
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+
+    def forward(self, X):
+
+        X0 = X[:,0:2].reshape(X.shape[0], 2, 512, 512)
+        X1 = X[:,2:4].reshape(X.shape[0], 2, 512, 512)
+        X2 = X[:,4:].reshape(X.shape[0], 2, 512, 512)
+
+        X0 = self.layer0(X0)
+        X0 = self.layer1(X0)
+        X0 = self.layer2(X0)
+        X0 = self.layer3(X0)
+        X0 = self.layer4(X0)
+        X0 = self.gap(X0)
+        X0 = X0.reshape(X0.shape[0],512)
+
+        X1 = self.layer0(X1)
+        X1 = self.layer1(X1)
+        X1 = self.layer2(X1)
+        X1 = self.layer3(X1)
+        X1 = self.layer4(X1)
+        X1 = self.gap(X1)
+        X1 = X1.reshape(X1.shape[0],512)
+
+        X2 = self.layer0(X2)
+        X2 = self.layer1(X2)
+        X2 = self.layer2(X2)
+        X2 = self.layer3(X2)
+        X2 = self.layer4(X2)
+        X2 = self.gap(X2)
+        X2 = X2.reshape(X2.shape[0],512)
+
+        X = torch.cat((X0, X1, X2), 1)
+
+        Xclass = nn.ReLU()(self.classBN1(self.fcClass1(X)))
+        Xclass = nn.ReLU()(self.classBN2(self.fcClass2(Xclass)))
+        Xclass = self.fcClass3(Xclass)
+
+        Xreg = nn.ReLU()(self.regBN1(self.fcReg1(X)))
+        Xreg = nn.ReLU()(self.regBN2(self.fcReg2(Xreg)))
+        Xreg = self.fcReg3(Xreg)
+
+        outputs = [self.logSoftmax(Xclass), nn.Sigmoid()(Xreg).squeeze()]
+        return outputs
+
+
+
+
 #other versions below not modified for multiTask    
 
 #class ResNet34Pl2(nn.Module):
--- train/train_wandb_reco.py
+++ train/train_wandb_reco.py
@@ -55,6 +55,8 @@ parser.add_argument("-p", "--projectName", type=str, default="prongCNN-5particle
 parser.add_argument("-r", "--runName", type=str, default="DEFAULT", help="wandb run name")
 parser.add_argument("-wLP", "--partLossWeight", type=float, default=0.5, help="weight for particle classification in multi task loss (must specify --multiTask and --hardWeights)")
 parser.add_argument("--multiTask", action="store_true", help="do particle classification and completeness regression")
+parser.add_argument("--deepMLP", action="store_true", help="use 3 layer MLPs for output tasks")
+parser.add_argument("--deepMLPwBN", action="store_true", help="use 3 layer MLPs with batch norm for output tasks")
 parser.add_argument("--classifyComp", action="store_true", help="do classification instead of regression for completeness")
 parser.add_argument("--hardWeights", action="store_true", help="use hard coded task weights for multi task loss")
 parser.add_argument("--use6class", action="store_true", help="use 6 classes (include other label)")
@@ -70,8 +72,11 @@ args = parser.parse_args()
 if args.multiTask and (args.l0inChans != 2 or args.use6class or args.softLabels or args.noMask or args.plane2only or args.resnet18):
   sys.exit("multiTask training only configured for 5 class hard labels with mask (3 plane, 2 in channel config.) with ResNet34")
 
+if ((args.deepMLP or args.deepMLPwBN) and not args.multiTask) or ((args.deepMLP or args.deepMLPwBN) and (args.classifyComp or args.hardWeights)):
+  sys.exit("deepMLP and deepMLPwBN options are only implemented for multi task config. with completeness regression and learnable loss weights")
+
 if args.multiTask:
-  from models_instanceNorm_reco_2chan_multiTask import ResBlock, ResNet34, ResNet34ClCmp
+  from models_instanceNorm_reco_2chan_multiTask import ResBlock, ResNet34, ResNet34ClCmp, ResNet34DeepMLP, ResNet34DeepMLPwBN
 elif args.noMask:
   from models_instanceNorm import ResBlock, ResNet18, ResNet18Pl2, ResNet34, ResNet34Pl2
 elif args.l0inChans == 1:
@@ -165,8 +170,15 @@ else:
     if args.resnet18:
         model = ResNet18(layer0inChans, ResBlock, outputs=nClasses)
     else:
-        if args.multiTask and args.classifyComp:
-            model = ResNet34ClCmp(layer0inChans, ResBlock, outputs=nClasses)
+        if args.multiTask:
+            if args.classifyComp:
+                model = ResNet34ClCmp(layer0inChans, ResBlock, outputs=nClasses)
+            elif args.deepMLP:
+                model = ResNet34DeepMLP(layer0inChans, ResBlock, outputs=nClasses)
+            elif args.deepMLPwBN:
+                model = ResNet34DeepMLPwBN(layer0inChans, ResBlock, outputs=nClasses)
+            else:
+                model = ResNet34(layer0inChans, ResBlock, outputs=nClasses)
         else:
             model = ResNet34(layer0inChans, ResBlock, outputs=nClasses)
     if not args.singleGPU:
